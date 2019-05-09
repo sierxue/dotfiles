ad9e14a91581a3ded8432ba0125cbc98ec26536b
sierxue <19348230+sierxue@users.noreply.github.com>
2019-05-10 00:37:07 +0800

    dotfiles: Deprecate some python settings by the dotbot author.

--- shell/external.sh
+++ shell/external.sh
@@ -1,11 +1,11 @@
 # pip should only run if there is a virtualenv currently activated
-export PIP_REQUIRE_VIRTUALENV=true
+# export PIP_REQUIRE_VIRTUALENV=true
 
 # Cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+# export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 
 # Python startup file
-export PYTHONSTARTUP=$HOME/.pythonrc
+# export PYTHONSTARTUP=$HOME/.pythonrc
 
 # Vagrant
-VAGRANT_DISABLE_VBOXSYMLINKCREATE=1
+# VAGRANT_DISABLE_VBOXSYMLINKCREATE=1
